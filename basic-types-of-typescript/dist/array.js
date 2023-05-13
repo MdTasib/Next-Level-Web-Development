cecf844f4449fad872959275df86b7931b7db07d
MdTasib <ohidulalam442@gmail.com>
2023-05-13 17:40:21 +0600

    array

---
+++ basic-types-of-typescript/dist/array.js
@@ -0,0 +1,7 @@
+"use strict";
+const names = ["Rafi", "Tasib", "Rakib"];
+const ages = [1, 2, 3];
+const array = ["name", 12, false]; // Multiples types
+// Tuple
+const tupleArray = ["Rafi Vayya", 25];
+// const wrongArray: [string, number]= [ 25, "Rafi Vayya"]; // error
---
+++ basic-types-of-typescript/src/array.ts
@@ -0,0 +1,7 @@
+const names: string[] = ["Rafi", "Tasib", "Rakib"];
+const ages: number[] = [1, 2, 3];
+const array = ["name", 12, false]; // Multiples types
+
+// Tuple
+const tupleArray: [string, number]= ["Rafi Vayya", 25];
+// const wrongArray: [string, number]= [ 25, "Rafi Vayya"]; // error
