f8b8f2693dc863e03a588eee98ed12600d054029
MdTasib <ohidulalam442@gmail.com>
2023-05-15 12:22:51 +0600

    setup file

--- advance-types-of-typescript/dist/generic-keyof-constraints.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// const a:newType = "hello"; // error
-// const a:newType = "name";
-// const a:newTypeUsingKeyOf = "hello"; // error
-// const b:newTypeUsingKeyOf = "name";
--- basic-types-of-typescript/dist/test.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const age = 'Test';
-console.log(age);
