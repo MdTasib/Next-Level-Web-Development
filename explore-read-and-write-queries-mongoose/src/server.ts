4be3e774f0aa293e83b35130199d633d30d966fd
MdTasib <ohidulalam442@gmail.com>
2023-05-17 20:05:21 +0600

    interface and schema

--- explore-read-and-write-queries-mongoose/src/server.ts
+++ explore-read-and-write-queries-mongoose/src/server.ts
@@ -1,28 +1,20 @@
 import mongoose from "mongoose";
-const express = require('express')
-const app = express()
-const port = 5000
+import app from './app';
 
-// database connection
+const port:number = 5000
+
+
 //database connection
 async function bootstrap() {
   try {
     await mongoose.connect("mongodb+srv://acc-inventory:acc-inventory@cluster0.aycinux.mongodb.net/acc-inventory");
     console.log(`🛢 Database connection successful`);
 
-    // app.listen(port, () => {
-    //   console.log(`Server is  listening on port ${port}`);
-    // });
+    app.listen(port, () => {
+      console.log(`Server is  listening on port ${port}`);
+    });
   } catch (err) {
     console.log(`Failed to connect database`, err);
   }
 }
 bootstrap();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
