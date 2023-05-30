d4b006f679793c2b9706afb38fa4c9f310be04a9
MdTasib <ohidulalam442@gmail.com>
2023-05-30 16:38:24 +0600

    Create generateUser utility function

--- university-management-system/src/modules/users/user.service.ts
+++ university-management-system/src/modules/users/user.service.ts
@@ -1,9 +1,19 @@
+import config from '../../config'
 import { IUser } from './user.interface'
 import { User } from './user.model'
+import { generateUserId } from './user.utils'
 
 const createUser = async (payload: IUser): Promise<IUser | null> => {
-  const user = await User.create(payload)
+  // auto generated incremental id
+  const id = await generateUserId()
+  payload.id = id
 
+  // check password
+  if (!payload.password) {
+    payload.password = config.default_user_pass as string
+  }
+
+  const user = await User.create(payload)
   if (!user) {
     throw new Error('Failed to create user')
   }
