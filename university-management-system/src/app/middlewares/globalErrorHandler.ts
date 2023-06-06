bc728c2189480b20240d797eca9456eb2c4f37c9
MdTasib <ohidulalam442@gmail.com>
2023-06-06 16:59:24 +0600

    Finish Your Error Handeling Setup

--- university-management-system/src/app/middlewares/globalErrorHandler.ts
+++ university-management-system/src/app/middlewares/globalErrorHandler.ts
@@ -1,10 +1,17 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
 import { ErrorRequestHandler } from 'express'
 import config from '../../config'
 import { IGenericErrorMessage } from '../../interfaces/error'
 import handleValidationError from '../../errors/handleValidationError'
 import ApiError from '../../errors/ApiError'
+import { errorLogger } from '../../shared/logger'
 
 const globalErrorHanlder: ErrorRequestHandler = (error, req, res, next) => {
+  config.node_env === 'development'
+    ? console.log('globalErrorHandler ~', error)
+    : errorLogger.error('globalErrorHanlder ~', error)
+
   let statusCode = 500
   let message = 'Something went wrong!'
   let errorMessages: IGenericErrorMessage[] = []
