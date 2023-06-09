7eb8c69f51dc8b8c90f908ef40352ae8aba77b47
MdTasib <ohidulalam442@gmail.com>
2023-06-09 22:20:53 +0600

    pagination, create semester controller

--- university-management-system/src/app.ts
+++ university-management-system/src/app.ts
@@ -1,7 +1,8 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
+import httpStatus from 'http-status';
 const app: Application = express();
 
 app.use(cors());
@@ -20,5 +21,15 @@ app.use('/api/v1', routes);
 
 //global error handler
 app.use(globalErrorHandler);
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [{ path: req.originalUrl, message: 'Api Not Found' }],
+  });
+  next();
+});
 
 export default app;
--- university-management-system/src/app/modules/academicSemester/academicSemester.controller.ts
+++ university-management-system/src/app/modules/academicSemester/academicSemester.controller.ts
@@ -22,6 +22,30 @@ const createSemester = catchAsync(
   }
 );
 
+const getAllSemesters = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const paginationOptions = {
+      page: Number(req.query.page),
+      limit: Number(req.query.limit),
+      sortBy: req.query.sortBy,
+      sortOrder: req.query.sortOrder,
+    };
+
+    const result = await AcademicSemesterService.getAllSemesters(
+      paginationOptions
+    );
+
+    sendResponse<IAcademicSemester>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semesters retirieved successfully!',
+      data: result,
+    });
+    next();
+  }
+);
+
 export const AcademicSemesterController = {
   createSemester,
+  getAllSemesters,
 };
