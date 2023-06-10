706108d51bfae2bd6060471c18bc67abb684db05
MdTasib <ohidulalam442@gmail.com>
2023-06-10 16:53:29 +0600

    Optimize your query object using pick

--- university-management-system/src/app/modules/academicSemester/academicSemester.controller.ts
+++ university-management-system/src/app/modules/academicSemester/academicSemester.controller.ts
@@ -4,6 +4,8 @@ import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemesterService } from './academicSemester.service';
+import pick from '../../../shared/pick';
+import { paginationFields } from '../../../constants/pagination';
 
 const createSemester = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -24,23 +26,19 @@ const createSemester = catchAsync(
 
 const getAllSemesters = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const paginationOptions = {
-      page: Number(req.query.page),
-      limit: Number(req.query.limit),
-      sortBy: req.query.sortBy,
-      sortOrder: req.query.sortOrder,
-    };
+    const paginationOptions = pick(req.query, paginationFields);
+    console.log(paginationOptions);
 
-    const result = await AcademicSemesterService.getAllSemesters(
-      paginationOptions
-    );
+    // const result = await AcademicSemesterService.getAllSemesters(
+    //   paginationOptions
+    // );
 
-    sendResponse<IAcademicSemester>(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Semesters retirieved successfully!',
-      data: result,
-    });
+    // sendResponse<IAcademicSemester>(res, {
+    //   statusCode: httpStatus.OK,
+    //   success: true,
+    //   message: 'Semesters retirieved successfully!',
+    //   data: result,
+    // });
     next();
   }
 );
