b0229d535038fa21a1d9c3a03974b60e8bbc0362
MdTasib <ohidulalam442@gmail.com>
2023-06-15 12:27:31 +0600

    15-3: Create generatefacultyId() utility and get some bug to enjoy

--- university-management-system/src/app/modules/user/user.utils.ts
+++ university-management-system/src/app/modules/user/user.utils.ts
@@ -2,21 +2,14 @@ import { IAcademicSemester } from '../academicSemester/academicSemester.interfac
 import { User } from './user.model';
 
 export const findLastStudentId = async (): Promise<string | undefined> => {
-  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+  const lastStudent = await User.findOne({}, { id: 1, _id: 0 })
     .sort({
       createdAt: -1,
     })
     .lean();
 
-  return lastUser?.id;
+  return lastStudent?.id;
 };
-
-// export const generateUserId = async (): Promise<string> => {
-//   const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0'); //00000
-//   //increment by 1
-//   const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0');
-//   return incrementedId;
-// };
 
 export const generateStudentId = async (
   academicSemester: IAcademicSemester
@@ -28,6 +21,23 @@ export const generateStudentId = async (
   incrementedId = `${academicSemester.year.substring(2)}${
     academicSemester.code
   }${incrementedId}`;
-  console.log(incrementedId);
+  return incrementedId;
+};
+
+export const findLastFacultyId = async (): Promise<string | undefined> => {
+  const lastFaculty = await User.findOne({}, { id: 1, _id: 0 })
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastFaculty?.id;
+};
+
+export const generateFacultyId = async (): Promise<string> => {
+  const currentId =
+    (await findLastFacultyId()) || (0).toString().padStart(5, '0');
+  let incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+  incrementedId = `F-${incrementedId}`;
   return incrementedId;
 };
